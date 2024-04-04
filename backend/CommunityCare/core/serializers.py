36166079b5855a01d106a92442a6b40753c5f387
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-04 12:15:46 +0800

    Add serializers

--- backend/CommunityCare/core/serializers.py
+++ backend/CommunityCare/core/serializers.py
@@ -1,1 +1,41 @@
-from rest_framework.serializers import Serializer
+from rest_framework import serializers
+from .models import Citizenship, Location, Family, Member, MaritalStatus, EducationalAttainment,EmployementStatus
+
+
+class LocationSerializer(serializers.Serializer):
+    class Meta:
+        model = Location
+        fields = ["address", "coordinates"]
+
+class FamilySerializer(serializers.Serializer):
+    class Meta:
+        model = Family
+        fields = ["title", "no_of_members", "total_family_income", "duration_of_residence",  "location"]
+        depth = 1
+
+class CitizenshipSerializer(serializers.Serializer):
+    class Meta:
+        model = Citizenship
+        fields = ["title"]
+
+class EducationalAttainmentSerializer(serializers.Serializer):
+    class Meta:
+        model = EducationalAttainment
+        fields = ["title"]
+
+class EmployementStatusSerializer(serializers.Serializer):
+    class Meta:
+        model = EmployementStatus
+        fields = ["title"]
+
+class MaritalStatusSerializer(serializers.Serializer):
+    class Meta:
+        model = MaritalStatus
+        fields = ["title"]
+
+class MemberSerializer(serializers.Serializer):
+
+    class Meta:
+        depth = 1
+        model = Member
+        fields = ["name", "age", "contact_no", "income", "birthdate", "birth_place", "marital_status", "citizenship", "educatinal_attainment", "employement_status", "family"]
