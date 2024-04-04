3186eb6b5a31dabb740c68c5b4e1a2fda0560432
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-04 12:15:34 +0800

    Register models to admin panel

--- backend/CommunityCare/core/admin.py
+++ backend/CommunityCare/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import Location, Family, Citizenship, EducationalAttainment, EmployementStatus, MaritalStatus, Member
 
-# Register your models here.
+admin.site.register(Location)
+admin.site.register(Family)
+admin.site.register(Citizenship)
+admin.site.register(EducationalAttainment)
+admin.site.register(EmployementStatus)
+admin.site.register(MaritalStatus)
+admin.site.register(Member)
+
