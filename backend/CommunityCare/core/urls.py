12b8257a121d50fdb026479968771856e65bbf0b
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-13 20:55:09 +0800

    Add all resource responses

--- backend/CommunityCare/core/urls.py
+++ backend/CommunityCare/core/urls.py
@@ -2,18 +2,124 @@ from django.urls import path, include
 from . import views
 
 
-app_name = "core"
 urlpatterns = [
-    path("docs", views.docs, name="docs"),
-    path("family", views.FamiliesViewSet.as_view({
-        "get":"list",
-        "post":"create"
-         
-         }), name="families"),
-    path("family/<int:pk>", views.SingleFamilyViewSet.as_view({
-        "get":"retrieve"
-    }), name="family"),
-
-
-
+    path(
+        "locations",
+        views.LocationsViewSet.as_view({"get": "list", "post": "create"}),
+        name="locations",
+    ),
+    path(
+        "locations/<int:pk>",
+        views.SingleLocationViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="location-detail",
+    ),
+    path(
+        "family",
+        views.FamiliesViewSet.as_view({"get": "list", "post": "create"}),
+        name="families",
+    ),
+    path(
+        "family/<int:pk>",
+        views.SingleFamilyViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="family-detail",
+    ),
+    path(
+        "citizenships",
+        views.CitizenshipsViewSet.as_view({"get": "list", "post": "create"}),
+        name="citizenships",
+    ),
+    path(
+        "citizenships/<int:pk>",
+        views.SingleCitizenshipViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="citizenship-detail",
+    ),
+    path(
+        "educational-attainments",
+        views.EducationalAttainmentsViewSet.as_view({"get": "list", "post": "create"}),
+        name="educational-attainments",
+    ),
+    path(
+        "educational-attainments/<int:pk>",
+        views.SingleEducationalAttainmentViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="educationalattainment-detail",
+    ),
+    path(
+        "employment-statuses",
+        views.EmploymentStatusesViewSet.as_view({"get": "list", "post": "create"}),
+        name="employment-statuses",
+    ),
+    path(
+        "employment-statuses/<int:pk>",
+        views.SingleEmploymentStatusViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="employmentstatus-detail",
+    ),
+    path(
+        "marital-statuses",
+        views.MaritalStatusesViewSet.as_view({"get": "list", "post": "create"}),
+        name="marital-statuses",
+    ),
+    path(
+        "marital-statuses/<int:pk>",
+        views.SingleMaritalStatusViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="maritalstatus-detail",
+    ),
+    path(
+        "members",
+        views.MembersViewSet.as_view({"get": "list", "post": "create"}),
+        name="members",
+    ),
+    path(
+        "members/<int:pk>",
+        views.SingleMemberViewSet.as_view(
+            {
+                "get": "retrieve",
+                "put": "update",
+                "patch": "partial_update",
+                "delete": "destroy",
+            }
+        ),
+        name="member-detail",
+    ),
 ]
--- backend/CommunityCare/core/views.py
+++ backend/CommunityCare/core/views.py
@@ -1,41 +1,72 @@
 from django.shortcuts import render
-from rest_framework.response import Response
-from rest_framework.renderers import JSONRenderer, StaticHTMLRenderer
-from rest_framework import status
-from rest_framework.viewsets import ViewSet
-from rest_framework.decorators import api_view
-from django.http import HttpResponsePermanentRedirect
-from .serializers import FamilySerializer
-from .models import Family
+from rest_framework.viewsets import ModelViewSet
+from .serializers import LocationSerializer, FamilySerializer, CitizenshipSerializer, MemberSerializer, EducationalAttainmentSerializer, EmploymentStatusSerializer, MaritalStatusSerializer, MemberSerializer
+from .models import Location, Family, Citizenship, EducationalAttainment, EmploymentStatus, MaritalStatus, Member
 
-@api_view()
-def docs(request):
-    return Response({"sample":"test"}, status=status.HTTP_200_OK)
+class LocationsViewSet(ModelViewSet):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
 
 
-class FamiliesViewSet(ViewSet):
-    def list(self, request, pk=None):
-        families = Family.objects.all()
-        
-        title = request.query_params.get("title")
-        if title:
-            families = families.filter(title__contains=title)
+class SingleLocationViewSet(ModelViewSet):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
 
-        serialized_item = FamilySerializer(families, many=True)
-        return Response(serialized_item.data, status=status.HTTP_200_OK)
-    
-    def create(self, request, pk=None):
-        deserialized_item = FamilySerializer(request.data)
-        if not deserialized_item.is_valid(raise_exception=True):
-            return Response(deserialized_item.errors, status=status.HTTP_400_BAD_REQUEST) 
-        return Response(deserialized_item.data, status=status.HTTP_201_CREATED)
-    
+class FamiliesViewSet(ModelViewSet):
+    serializer_class = FamilySerializer
+    queryset = Family.objects.all()
 
 
-class SingleFamilyViewSet(ViewSet):
-    def retrieve(self, request, pk=None):
+class SingleFamilyViewSet(ModelViewSet):
+    serializer_class = FamilySerializer
+    queryset = Family.objects.all()
 
-        family = Family.objects.get(pk=pk)
-        serialized_item = FamilySerializer(family)
-        return Response(serialized_item.data, status=status.HTTP_200_OK)
-    
+class CitizenshipsViewSet(ModelViewSet):
+    serializer_class = CitizenshipSerializer
+    queryset = Citizenship.objects.all()
+
+
+class SingleCitizenshipViewSet(ModelViewSet):
+    serializer_class = CitizenshipSerializer
+    queryset = Citizenship.objects.all()
+
+class CitizenshipsViewSet(ModelViewSet):
+    serializer_class = CitizenshipSerializer
+    queryset = Citizenship.objects.all()
+
+
+class SingleCitizenshipViewSet(ModelViewSet):
+    serializer_class = CitizenshipSerializer
+    queryset = Citizenship.objects.all()
+
+class EducationalAttainmentsViewSet(ModelViewSet):
+    serializer_class = EducationalAttainmentSerializer
+    queryset = EducationalAttainment.objects.all()
+
+class SingleEducationalAttainmentViewSet(ModelViewSet):
+    serializer_class = EducationalAttainmentSerializer
+    queryset = EducationalAttainment.objects.all()
+
+class EmploymentStatusesViewSet(ModelViewSet):
+    serializer_class = EmploymentStatusSerializer
+    queryset = EmploymentStatus.objects.all()
+
+class SingleEmploymentStatusViewSet(ModelViewSet):
+    serializer_class = EmploymentStatusSerializer
+    queryset = EmploymentStatus.objects.all()
+
+class MaritalStatusesViewSet(ModelViewSet):
+    serializer_class = MaritalStatusSerializer
+    queryset = MaritalStatus.objects.all()
+
+class SingleMaritalStatusViewSet(ModelViewSet):
+    serializer_class = MaritalStatusSerializer
+    queryset = MaritalStatus.objects.all()
+
+class MembersViewSet(ModelViewSet):
+    serializer_class = MemberSerializer
+    queryset = Member.objects.all()
+
+class SingleMemberViewSet(ModelViewSet):
+    serializer_class = MemberSerializer
+    queryset = Member.objects.all()
