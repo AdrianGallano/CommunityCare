c90fb83b2b88127fe6edefc313fccd6d6d0efd53
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-14 00:10:10 +0800

    Add deployment configurations

--- backend/CommunityCare/CommunityCare/settings.py
+++ backend/CommunityCare/CommunityCare/settings.py
@@ -27,9 +27,11 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
