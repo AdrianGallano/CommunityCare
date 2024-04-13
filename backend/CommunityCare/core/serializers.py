9dbcc1eec1d8c8d12624d29c90f7553802b0e707
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-13 20:54:05 +0800

    Update serializers

--- backend/CommunityCare/core/serializers.py
+++ backend/CommunityCare/core/serializers.py
@@ -1,41 +1,106 @@
 from rest_framework import serializers
-from .models import Citizenship, Location, Family, Member, MaritalStatus, EducationalAttainment,EmployementStatus
+from .models import (
+    Citizenship,
+    Location,
+    Family,
+    Member,
+    MaritalStatus,
+    EducationalAttainment,
+    EmploymentStatus,
+)
 
 
-class LocationSerializer(serializers.ModelSerializer):
+class LocationSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Location
-        fields = ["address", "coordinates"]
+        fields = ["id", "address", "coordinates"]
 
-class FamilySerializer(serializers.ModelSerializer):
+
+class FamilySerializer(serializers.HyperlinkedModelSerializer):
+    location = LocationSerializer(read_only=True)
+    location_id = serializers.PrimaryKeyRelatedField(
+        queryset=Location.objects.all(), source="location", write_only=True
+    )
+
     class Meta:
         model = Family
-        fields = ["title", "no_of_members", "total_family_income", "duration_of_residence",  "location"]
+        fields = [
+            "id",
+            "title",
+            "no_of_members",
+            "total_family_income",
+            "duration_of_residence",
+            "location",
+            "location_id",
+        ]
         depth = 1
 
-class CitizenshipSerializer(serializers.ModelSerializer):
+
+class CitizenshipSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Citizenship
-        fields = ["title"]
+        fields = ["id", "title"]
 
-class EducationalAttainmentSerializer(serializers.ModelSerializer):
+
+class EducationalAttainmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = EducationalAttainment
-        fields = ["title"]
+        fields = ["id", "title"]
 
-class EmployementStatusSerializer(serializers.ModelSerializer):
+
+class EmploymentStatusSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = EmployementStatus
-        fields = ["title"]
+        model = EmploymentStatus
+        fields = ["id", "title"]
 
-class MaritalStatusSerializer(serializers.ModelSerializer):
+
+class MaritalStatusSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = MaritalStatus
-        fields = ["title"]
+        fields = ["id", "title"]
 
-class MemberSerializer(serializers.ModelSerializer):
+
+class MemberSerializer(serializers.HyperlinkedModelSerializer):
+    marital_status_id = serializers.PrimaryKeyRelatedField(
+        queryset=MaritalStatus.objects.all(), source="marital_status", write_only=True
+    )
+    citizenship_id = serializers.PrimaryKeyRelatedField(
+        queryset=Citizenship.objects.all(), source="citizenship", write_only=True
+    )
+    educational_attainment_id = serializers.PrimaryKeyRelatedField(
+        queryset=EducationalAttainment.objects.all(),
+        source="educational_attainment",
+        write_only=True,
+    )
+    employment_status_id = serializers.PrimaryKeyRelatedField(
+        queryset=EmploymentStatus.objects.all(),
+        source="employment_status",
+        write_only=True,
+    )
+    family_id = serializers.PrimaryKeyRelatedField(
+        queryset=Family.objects.all(), source="family", write_only=True, many=True
+    )
 
     class Meta:
         depth = 1
         model = Member
-        fields = ["name", "age", "contact_no", "income", "birthdate", "birth_place", "marital_status", "citizenship", "educatinal_attainment", "employement_status", "family"]
+        fields = [
+            "id",
+            "name",
+            "age",
+            "contact_no",
+            "income",
+            "gender",
+            "birthdate",
+            "birth_place",
+            "marital_status",
+            "citizenship",
+            "educational_attainment",
+            "employment_status",
+            "family",
+            "marital_status_id",
+            "citizenship_id",
+            "educational_attainment_id",
+            "employment_status_id",
+            "family_id",
+        ]
