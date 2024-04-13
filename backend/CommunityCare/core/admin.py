ffa5a387b10834ed379d53416b439a716a64ae31
Adrian Gallano <adriangallanoengr@gmail.com>
2024-04-13 20:53:22 +0800

    Update models

--- backend/CommunityCare/core/admin.py
+++ backend/CommunityCare/core/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
-from.models import Location, Family, Citizenship, EducationalAttainment, EmployementStatus, MaritalStatus, Member
+from.models import Location, Family, Citizenship, EducationalAttainment, EmploymentStatus, MaritalStatus, Member
 
 admin.site.register(Location)
 admin.site.register(Family)
 admin.site.register(Citizenship)
 admin.site.register(EducationalAttainment)
-admin.site.register(EmployementStatus)
+admin.site.register(EmploymentStatus)
 admin.site.register(MaritalStatus)
 admin.site.register(Member)
 
--- backend/CommunityCare/core/models.py
+++ backend/CommunityCare/core/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+GENDER_CHOICES = (
+    ("Male", "Male"),
+    ("Female", "Female"),
+    ("Non-binary", "Non-binary"),
+    ("Other", "Other"),
+    ("Prefer not to say", "Prefer not to say"),
+)
+
 class Location(models.Model):
     coordinates = models.CharField(max_length=100)
     address = models.CharField(max_length=500)
@@ -44,7 +52,7 @@ class EducationalAttainment(models.Model):
     class Meta:
         indexes = [models.Index(fields=["title"])]
 
-class EmployementStatus(models.Model):
+class EmploymentStatus(models.Model):
     title = models.CharField(max_length=255)
     
     def __str__(self):
@@ -62,17 +70,20 @@ class MaritalStatus(models.Model):
     class Meta:
         indexes = [models.Index(fields=["title"])]
 
+
+
 class Member(models.Model):
     name = models.CharField(max_length=255, )
     age = models.IntegerField()
-    contact_no = models.CharField(max_length=11)
+    contact_no = models.CharField(max_length=20)
     income  = models.DecimalField(max_digits=10 ,decimal_places=2)
+    gender = models.CharField(max_length=30, null=True, choices=GENDER_CHOICES)
     birthdate = models.DateField()
     birth_place = models.CharField(max_length=255)
-    marital_status = models.OneToOneField(MaritalStatus, on_delete=models.CASCADE)
-    citizenship = models.OneToOneField(Citizenship, on_delete=models.CASCADE)
-    educational_attainment = models.OneToOneField(EducationalAttainment, on_delete=models.CASCADE)
-    employement_status = models.OneToOneField(EmployementStatus, on_delete=models.CASCADE)
+    marital_status = models.ForeignKey(MaritalStatus, on_delete=models.CASCADE)
+    citizenship = models.ForeignKey(Citizenship, on_delete=models.CASCADE)
+    educational_attainment = models.ForeignKey(EducationalAttainment, on_delete=models.CASCADE)
+    employment_status = models.ForeignKey(EmploymentStatus, on_delete=models.CASCADE)
     family = models.ManyToManyField(Family)
 
     def __str__(self):
